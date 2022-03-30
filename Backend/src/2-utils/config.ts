ef2babbf71a836d23e1add80bbde11b35a25345c
Pirate-Emperor <piratekingrahul@gmail.com>
2022-03-30 01:18:00 +0100

    Add Backend\src\2-utils\config.ts

---
+++ Backend/src/2-utils/config.ts
@@ -0,0 +1,8 @@
+class Config {
+    public connectionString = "mongodb+srv://mongodbcom:03vkHeJ4aqrRZ8De@shopping-online.jwqcg.mongodb.net/Shopping-Online?retryWrites=true&w=majority&appName=Shopping-Online"; //* Fill in the blank (database name)...
+    public port = 3001;
+}
+
+const config = new Config();
+
+export default config;

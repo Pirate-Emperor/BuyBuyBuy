545df2de7c57eb9bb23fe3bf6f931f6a0e933a43
Pirate-Emperor <piratekingrahul@gmail.com>
2022-04-03 15:01:00 +0100

    Add Backend\src\4-models\city-enum.ts

---
+++ Backend/src/4-models/city-enum.ts
@@ -0,0 +1,14 @@
+enum CityEnum {
+    Jerusalem = "Jerusalem",
+    TelAviv = "Tel Aviv",
+    Haifa = "Haifa",
+    RishonLeZion = "Rishon LeZion",
+    PetahTikva = "Petah Tikva",
+    Ashdod = "Ashdod",
+    Netanya = "Netanya",
+    Beersheva = "Beer Sheva",
+    BneiBrak = "Bnei Brak",
+    Holon = "Holon"
+}
+
+export default CityEnum

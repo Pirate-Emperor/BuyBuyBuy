2a79a79ec33ce10e43f66da0d73c1bc0a50b3183
Pirate-Emperor <piratekingrahul@gmail.com>
2022-04-23 07:36:00 +0100

    Add Frontend\src\app\components\auth-area\auth-menu\auth-menu.component.ts

---
+++ Frontend/src/app/components/auth-area/auth-menu/auth-menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Unsubscribe } from 'redux';
+import { UserModel } from 'src/app/models/user.model';
+import { authStore } from 'src/app/redux/auth.state';
+
+@Component({
+    selector: 'app-auth-menu',
+    templateUrl: './auth-menu.component.html',
+    styleUrls: ['./auth-menu.component.css']
+})
+
+export class AuthMenuComponent implements OnInit, OnDestroy {
+    public user: UserModel;
+    private unsubscribe: Unsubscribe;
+
+    ngOnInit(): void {
+        this.user = authStore.getState().user;
+        this.unsubscribe = authStore.subscribe(() => {
+            this.user = authStore.getState().user;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe();
+    }
+}

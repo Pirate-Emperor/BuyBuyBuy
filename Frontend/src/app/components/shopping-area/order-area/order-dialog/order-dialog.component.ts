64d9bc5597cf9e74d10f6ae969b53f96836f0744
Pirate-Emperor <piratekingrahul@gmail.com>
2022-05-09 18:06:00 +0100

    Add Frontend\src\app\components\shopping-area\order-area\order-dialog\order-dialog.component.ts

---
+++ Frontend/src/app/components/shopping-area/order-area/order-dialog/order-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-order-dialog',
+    templateUrl: './order-dialog.component.html',
+    styleUrls: ['./order-dialog.component.css']
+})
+
+export class OrderDialogComponent { };

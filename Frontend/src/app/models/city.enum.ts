d910f7a68b29a0c187bbb7262ad1360d8d1a8d4d
Pirate-Emperor <piratekingrahul@gmail.com>
2022-05-18 13:37:00 +0100

    Add Frontend\src\app\models\city.enum.ts

---
+++ Frontend/src/app/models/city.enum.ts
@@ -0,0 +1,12 @@
+export enum CityEnum {
+    Jerusalem = "Jerusalem",
+    TelAviv = "Tel Aviv",
+    Haifa = "Haifa",
+    RishonLeZion = "Rishon LeZion",
+    PetahTikva = "Petah Tikva",
+    Ashdod = "Ashdod",
+    Netanya = "Netanya",
+    Beersheva = "Beer Sheva",
+    BneiBrak = "Bnei Brak",
+    Holon = "Holon"
+};

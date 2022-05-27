ae7f0d7e5403f27dadc00f79fb5fc2419bd14b24
Pirate-Emperor <piratekingrahul@gmail.com>
2022-05-27 10:54:00 +0100

    Add Frontend\src\environments\environment.prod.ts

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,25 @@
+export const environment = {
+    production: true,
+    
+    // auth: 
+    emailAndIdNumberUniqueUrl: 'https://shopping-online-xzr1.onrender.com/api/auth/check-if-email-or-id-is-unique/',
+    registerUrl: 'https://shopping-online-xzr1.onrender.com/api/auth/register/',
+    loginUrl: 'https://shopping-online-xzr1.onrender.com/api/auth/login/',
+
+    // categories: 
+    categoriesUrl: 'https://shopping-online-xzr1.onrender.com/api/categories/',
+
+    // products: 
+    productsUrl: 'https://shopping-online-xzr1.onrender.com/api/products/',
+    productsImageUrl: 'https://shopping-online-xzr1.onrender.com/shopping/images/',
+
+    //carts
+    cartByUserUrl: 'https://shopping-online-xzr1.onrender.com/api/cart-by-user/',  //to display when user's cart was createdAt 
+
+    // cart items 
+    cartItemsUrl: 'https://shopping-online-xzr1.onrender.com/api/items/',
+    cartItemsByCartUrl: 'https://shopping-online-xzr1.onrender.com/api/items-by-cart/',  //to display all items in user's current cart
+
+    //  orders: 
+    ordersUrl: 'https://shopping-online-xzr1.onrender.com/api/orders/'
+};
